496c27d8ed59ccb2996b5a77a6c0c18ae19fee72
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-05-08 15:42:42 +0100

    Créer validators

---
+++ backend/validators/userValidator.js
@@ -0,0 +1,22 @@
+const { body, validationResult } = require('express-validator');
+
+const userValidator = [
+  body('username').notEmpty().withMessage('Username is required')
+    .isLength({ min: 3 }).withMessage('Username needs to be 3+ chars'),
+
+  body('email').notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Email needs to be valid'),
+
+  body('password').notEmpty().withMessage('Password is required')
+    .isLength({ min: 4 }).withMessage('Password needs to be 4+ chars'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+module.exports = userValidator;
